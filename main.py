cc3b971c1c9a4d69c0fbaa4a6cac15a4c8ed0a0c
Niklas Reimer <niklas.reimer@luebeck.dlrg.de>
2022-05-22 14:35:52 +0100

    Add different thresholds for both devices. Added less post delay. Removed calib function

--- main.py
+++ main.py
@@ -33,47 +33,24 @@ class AudioChanelRecorder:
     StatusText="Not Started"
 
     def write_auto(self, datastream, savePostfix):
-        print(self.postfix+" saving data")
+        print("\n"+self.postfix+" saving data")
         filename = "file"+'{0:%Y-%m-%d_%H-%M-%S}'.format(datetime.datetime.now()) + savePostfix + ".mp3"
 
         segment=AudioSegment(datastream, sample_width=self.audio.get_sample_size(self.FORMAT), channels=self.CHANNELS, frame_rate=self.RATE)
         segment = effects.normalize(segment)
         segment.export(filename, format='mp3', bitrate='128')
         
-        print(self.postfix+" saving data done")
+        print("\n"+self.postfix+" saving data done")
 
-    def makeNoiseCal(self, audioStream):
-        samples = []
-
-        calibrationSamples=15
-        for i in range(calibrationSamples):
-            sys.stdout.write("\rCalibrating: "+str(int(100*i/calibrationSamples))+"%")
-            sys.stdout.flush()
-            data=audioStream.read(self.CHUNK, exception_on_overflow = False)
-            snd_data = array('h', data)
-            if byteorder == 'big':
-                snd_data.byteswap()
-            samples.append(np.average(np.absolute(snd_data)))
-
-
-        print("Samples: "+str(samples))
-        print("Mean: "+str(np.average(samples)))
-        print("Std: "+str(np.std(samples)))
-
-        sigma=6
-        threshold=np.average(samples)+sigma*np.std(samples)
-        print(self.postfix+" Threshold: "+str(threshold))
-        return threshold
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 44100
     CHUNK = 4096
-    RECORD_SECONDS = 5
     postfix=""
 
     audio = pyaudio.PyAudio()
 
-    def Run(self, deviceIndex, savePostfix):
+    def Run(self, deviceIndex, savePostfix, gateThreshold):
         self.postfix=savePostfix
         # start Recording
         stream = self.audio.open(format=self.FORMAT, channels=self.CHANNELS,
@@ -84,9 +61,6 @@ class AudioChanelRecorder:
 
         recordStarted=False
         lastNoise=time.time()
-
-        #gateThreshold = self.makeNoiseCal(stream)
-        gateThreshold = 750
 
         while 1:
             data=stream.read(self.CHUNK, exception_on_overflow = False)
@@ -104,7 +78,7 @@ class AudioChanelRecorder:
 
             if(recordStarted):
                 frames.append(np.array(snd_data))
-                if(time.time()-lastNoise>3):
+                if(time.time()-lastNoise>1):
                     print("\n"+self.postfix+" Stop recording")
                     recordStarted=False
                     Thread(target=self.write_auto, args=(b''.join(frames), savePostfix)).start()
@@ -121,8 +95,8 @@ def main():
     device1=AudioChanelRecorder()
     device2=AudioChanelRecorder()
 
-    Thread(target=device1.Run, args=(1,"out")).start()
-    Thread(target=device2.Run, args=(2,"in")).start()
+    Thread(target=device1.Run, args=(1,"out",1500)).start()
+    Thread(target=device2.Run, args=(2,"in",750)).start()
 
     while 1:
         time.sleep(0.2)
